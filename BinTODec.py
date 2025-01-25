6f322bdd7a48d3ec539af2c9430fd4df3585e894
rudra pratap singh <prataprudra10102001@gmail.com>
2025-01-25 15:13:59 +0530

    Bin to Decimal

--- BinTODec.py
+++ BinTODec.py
@@ -1,1 +1,9 @@
-BinNum=input("Enter The Binary Number")
+BinNum=input("Enter The Binary Number: ")
+n=len(BinNum)
+# print(n)
+num=0
+for i in range(n):
+    digit=int(BinNum[n-i-1])
+    # print(digit)
+    num=num+(2**i)*digit
+print("Decimal value is :",num)
